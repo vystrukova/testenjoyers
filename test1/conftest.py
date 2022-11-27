56cceb5b47c231fe23857e3a58ab67c348781808
Evgeniy Folomeev <e.folomeev@vk.team>
2022-11-27 23:17:00 +0300

    fix

--- test1/conftest.py
+++ test1/conftest.py
@@ -2,11 +2,19 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument('--no-sandbox')  # avoid ci crash
+chrome_options.add_argument('--disable-gpu')  # avoid ci crash
+chrome_options.add_argument('--disable-dev-shm-usage')  # avoid ci crash (can add more shm-size in runner instead)
 
 
 @pytest.fixture()
 def browser():
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     browser.maximize_window()
     yield browser
     browser.quit()
